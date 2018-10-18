f24a47867c2fc25523b7e14767f22b8e40a10e2e
IsabelaGantzel <40005017+IsabelaGantzel@users.noreply.github.com>
2018-10-18 20:28:15 -0300

    Editar #24 Deletar #26 Listar #27
    Foi feito os Cruds dos usuários.

--- reserva-sala-back/src/controlers/Controller-Usuarios.js
+++ reserva-sala-back/src/controlers/Controller-Usuarios.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const Usuario = mongoose.model('Usuario');
 
 exports.get = (req, res, next) => {
-    Usuario.find({})
+    Usuario
+        .find({
+           active: true 
+        }, 'nome email departamento')
         .then(data => {
             res.status(201).send(data);
         }).catch(e => {
@@ -19,21 +22,42 @@ exports.post = (req, res, next) => {
         .save()
         .then(x => {
             res.status(201).send({
-                message: 'cadastrado'
+                message: 'usuário cadastrado com sucesso'
             });
         }).catch(e => {
             res.status(400).send({
-                message: 'falha',
+                message: 'falha ao cadastrar usuário',
                 data: e
             });
         });
 
 };
 
+exports.put = (req, res, next) => {
+    usuario
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                nome: req.body.nome,
+                email: req.body.email,
+                departamento: req.body.departamento,
+                senha1: req.body.senha1,
+                senha2: req.body.senha2
+            }
+        }).then(x => {
+            res.status(201).send({
+                message: 'Usuário atualizado com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao atualizar usuário!'
+                data: e
+            });
+        });
+    
+};
+
 exports.delete = (req, res, next) => {
-    Usuario.findOneAndRemove(
-        req.body.id
-    )
+    Usuario.findOneAndRemove(req.body.id)
     .then(data => {
         res.status(201).send({
             message: 'Usuario removido com sucesso!'
